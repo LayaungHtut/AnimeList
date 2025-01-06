7ffafce33b298e9e7eb6615af7e3b7a7762c5043
LaYaungHtut <layaunghtut5@gmail.com>
2025-01-06 22:42:31 +0630

    ERRORRR??? T_T

---
+++ src/lib/components/Spinner.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+	import type { AnimeProp } from '$lib';
+	import { fetchNewData } from '$lib/utils';
+	import AnimeCard from './AnimeCard.svelte';
+	let pagenumber = $state(1);
+
+	let ancestor: HTMLElement;
+
+	let options = {
+		rootMargin: '0px',
+		threshold: 1.0
+	};
+
+	let fetching = $state(false);
+	let animeArray: AnimeProp[] = $state([]);
+
+	async function getNewData() {
+		pagenumber = pagenumber + 1;
+		fetching = true;
+		const request = await fetchNewData(pagenumber);
+		try {
+			request.animes.forEach((anime) => {
+				animeArray.push(anime);
+			});
+			fetching = false;
+		} catch (error) {
+			console.log('error');
+		}
+	}
+	$effect(() => {
+		if (ancestor) {
+			let observer = new IntersectionObserver(() => {
+				getNewData(), options;
+			});
+			observer.observe(ancestor);
+		}
+	});
+</script>
+
+<section class="anime-grid">
+	{#if animeArray.length !== 0}
+		{#each animeArray as anime, i}
+			<AnimeCard {...anime} />
+		{/each}
+	{/if}
+</section>
+
+<div class="ancestor" bind:this={ancestor}>
+	{#if fetching}
+		<div class="spinner-container">
+			<img src="./spinner.svg" alt="spinner" width="56" height="56" class="spinner" />
+		</div>
+	{/if}
+</div>
+
+<style>
+	.anime-grid {
+		display: grid;
+		gap: 2.5rem;
+		grid-template-columns: repeat(3, 1fr);
+	}
+
+	.ancestor {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+
+		& .spinner-container {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+
+			& .spinner {
+				object-fit: contain;
+			}
+		}
+	}
+
+	@media (max-width: 768px) {
+		.anime-grid {
+			grid-template-columns: repeat(2, 1fr);
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,66 +1,59 @@
 <script lang="ts">
 	import AnimeCard from '$lib/components/AnimeCard.svelte';
+	import Spinner from '$lib/components/Spinner.svelte';
 
-
-    let { data } = $props();
-
+	let { data } = $props();
 </script>
 
 <div class="container">
-    <main class="main">
-      <h2 class="title">Explore Anime</h2>
-      <section class="anime-grid">
-
-    {#each data.animes as anime }
-        <AnimeCard {...anime} />
-    {/each}
-
-</section>
-<div id="spinner-container">
-  
-</div>
-</main>
+	<main class="main">
+		<h2 class="title">Explore Anime</h2>
+		<section class="anime-grid">
+			{#each data.animes as anime}
+				<AnimeCard {...anime} />
+			{/each}
+			<Spinner />
+		</section>
+		<div id="spinner-container"></div>
+	</main>
 </div>
 
 <style>
-    .container {
-      max-width: 80rem;
-      margin: 0 auto;
-      background-color: #0F1117;
-    }
-    .main {
-      padding: 4rem 2rem;
-      display: flex;
-      flex-direction: column;
-      gap: 2.5rem;
-    }
-    .title {
-      font-size: 1.875rem;
-      color: white;
-      font-weight: bold;
-    }
-    .anime-grid {
-      display: grid;
-      gap: 2.5rem;
-    }
-    @media (min-width: 640px) {
-      .anime-grid {
-        grid-template-columns: repeat(2, 1fr);
-      }
-    }
-    @media (min-width: 768px) {
-      .anime-grid {
-        grid-template-columns: repeat(3, 1fr);
-      }
-    }
-    @media (min-width: 1024px) {
-      .anime-grid {
-        grid-template-columns: repeat(4, 1fr);
-      }
-    }
-    #spinner-container {
-      text-align: center;
-    }
-  </style>
+	.container {
+		max-width: 80rem;
+		margin: 0 auto;
+		background-color: #0f1117;
 
+		& .main {
+			padding: 4rem 2rem;
+			display: flex;
+			flex-direction: column;
+			gap: 2.5rem;
 
+			& .title {
+				font-size: 1.875rem;
+				color: white;
+				font-weight: bold;
+			}
+
+			& .anime-grid {
+				display: grid;
+				gap: 2.5rem;
+				grid-template-columns: repeat(3, 1fr);
+			}
+		}
+	}
+
+	@media (max-width: 768px) {
+		.container {
+			& .main {
+				& .anime-grid {
+					grid-template-columns: repeat(2, 1fr);
+				}
+			}
+		}
+	}
+	#spinner-container {
+		text-align: center;
+	}
+</style>
